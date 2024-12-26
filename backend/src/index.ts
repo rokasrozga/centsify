a23ceea511c1cee3000fb66511be4e0da50a8a51
Rokas Rozga <rokasrozgadev@gmail.com>
2024-12-26 09:11:02 +0000

    Updated index.ts to first connect to DB then start express server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import "reflect-metadata"
 import express from "express"
 import dotenv from "dotenv"
 import router from "./routes/routes"
+import { database } from "./lib/db"
 
 dotenv.config()
 const app = express()
@@ -9,6 +10,13 @@ const app = express()
 app.use(express.json())
 app.use("/api", router)
 
-app.listen(process.env.SERVER_PORT, () => {
-    console.log(`Express server started on port: ${process.env.SERVER_PORT}`)
+database.initialize()
+.then(() => {
+    console.log("Database connected")
+    app.listen(process.env.SERVER_PORT, () => {
+        console.log(`Express server started on port: ${process.env.SERVER_PORT}`)
+    })
+})
+.catch(error => {
+    console.log(error)
 })
