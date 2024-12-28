03de87401476b4824b82bc82f04525a34d7468ed
Rokas Rozga <rokasrozgadev@gmail.com>
2024-12-28 22:45:37 +0000

    init - frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Dashboard from './pages/Dashboard'
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="*" element={<h1>Not Found</h1>} />
+    </Routes>
+  )
+}
+
+export default App
