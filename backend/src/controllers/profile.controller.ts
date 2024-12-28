4789cbd28b615c2e3b52685d3cb227a1cd084a62
Rokas Rozga <rokasrozgadev@gmail.com>
2024-12-28 20:08:44 +0000

    profile controller skeleton

---
+++ backend/src/controllers/profile.controller.ts
@@ -0,0 +1,17 @@
+// For adding Google login to an existing account
+export const addGoogleUser = async (req: Request, res: Response): Promise<void> => {
+
+}
+
+
+export const changePassword = async (req: Request, res: Response): Promise<void> => { 
+
+}
+
+export const updateProfilePicture = async (req: Request, res: Response): Promise<void> => { 
+
+}
+
+export const updateEmail = async (req: Request, res: Response): Promise<void> => {
+    
+}
