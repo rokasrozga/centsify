8601f652ff54b72bebfaad841cdc57f8d1f12a40
Rokas Rozga <rokasrozgadev@gmail.com>
2024-12-29 00:29:58 +0000

    Started route handling on frontend

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import Sidebar from '../components/Sidebar'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import Analytics from './Analytics'
+import Budgets from './Budgets'
+import Expenses from './Expenses'
+import Income from './Income'
+import SavingGoals from './SavingGoals'
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div>
+      <Sidebar />
+      <Routes>
+        <Route path="analytics" element={<Analytics />} />
+        <Route path='budgets' element={<Budgets />}/>
+        <Route path='expenses' element={<Expenses />}/>
+        <Route path='income' element={<Income />}/>
+        <Route path='saving-goals' element={<SavingGoals />}/>
+        <Route path='*' element={<Navigate to={'/dashboard'}/>} />
+      </Routes>
+    </div>
   )
 }
 
